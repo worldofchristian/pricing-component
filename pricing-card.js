3d8fd9f11972c03b3692c7075517a34778781266
Christian Waters <102264428+worldofchristian@users.noreply.github.com>
2023-03-05 21:07:49 -0600

    Add files via upload

---
+++ pricing-card.js
@@ -0,0 +1,59 @@
+<div
+className='shadow-xl rounded-2xl w-full p-6 mt-2 max-w-sm'>
+    <h1 
+    className="text-xl mt-2 font-medium tracking-wide">
+    Pro</h1>
+    <h1 
+    className="text-5xl font-bold tracking-wide">
+    $35
+    <span
+    className='text-sm font-medium'>/month</span></h1>
+    
+    <div
+    className='flex-col mt-4'>
+        <div
+        className='flex mt-2 gap-4 items-center text-center'>
+            <svg 
+            className='max-w-[11%]'
+            fill="none" stroke="currentColor" strokeWidth={2} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <h2 
+            className="text-sm font-medium tracking-wide"
+            >Feature</h2>
+        </div>
+        <div
+        className='flex mt-2 gap-4 items-center text-center'>
+            <svg 
+            className='max-w-[11%]'
+            fill="none" stroke="currentColor" strokeWidth={2} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <h2 
+            className="text-sm font-medium tracking-wide"
+            >Feature</h2>
+        </div>
+        <div
+        className='flex mt-2 gap-4 items-center text-center'>
+            <svg 
+            className='max-w-[11%]'
+            fill="none" stroke="currentColor" strokeWidth={2} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <h2 
+            className="text-sm font-medium tracking-wide"
+            >Feature </h2>
+        </div>
+        <div
+        className='flex mt-2 gap-4 items-center text-center'>
+            <svg 
+            className='max-w-[11%]'
+            fill="none" stroke="currentColor" strokeWidth={2} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <h2 
+            className="text-sm font-medium tracking-wide"
+            >Feature</h2>
+        </div>
+    </div>
+</div>
